3e71bd983593d2e74088d278702c2109185e0ec6
yaourtpourtoi <oleg.filatov@phystech.edu>
2021-08-07 17:41:42 +0200

    extend predict.py to log csv for evaluation

--- predict.py
+++ predict.py
@@ -9,11 +9,13 @@ import ROOT as R
 import uproot
 from lumin.nn.data.fold_yielder import FoldYielder
 import numpy as np
+import pandas as pd
+import mlflow
 
 from utils.processing import fill_placeholders
 from utils.inference import load_models, predict_folds
 
-@hydra.main(config_path="configs", config_name="predict")
+@hydra.main(config_path="configs/predict")
 def main(cfg: DictConfig) -> None:
     # fill placeholders in the cfg parameters
     input_path = to_absolute_path(fill_placeholders(cfg.input_path, {'{year}': cfg.year}))
@@ -32,30 +34,44 @@ def main(cfg: DictConfig) -> None:
     for s in model_cfg['signature']['inputs'].split('{')[1:]:
         train_features.append(s.split("\"")[3])
 
-    # loop over input fold files
-    for sample_name in cfg.sample_names:
-        print(f'\n--> Predicting {sample_name}')
-        print(f"        loading data set")
-        input_filename = fill_placeholders(cfg.input_filename_template, {'{sample_name}': sample_name, '{year}': cfg.year})
+    mlflow.set_tracking_uri(f"file://{to_absolute_path('mlruns')}")
+    with mlflow.start_run(run_id=cfg.mlflow_runID):
+        # loop over input fold files
+        for sample_name in cfg.sample_names:
+            print(f'\n--> Predicting {sample_name}')
+            print(f"        loading data set")
+            input_filename = fill_placeholders(cfg.input_filename_template, {'{sample_name}': sample_name, '{year}': cfg.year})
 
-        # extract DataFrame from fold file
-        fy = FoldYielder(f'{input_path}/{input_filename}')
-        df = fy.get_df(inc_inputs=True, deprocess=False, nan_to_num=False, verbose=False, suppress_warn=True)
-        for f in misc_features: # add misc features
-            df[f] = fy.get_column(f)
-        df['fold_id'] = df[xtrain_split_feature] % n_splits
+            # extract DataFrame from fold file
+            fy = FoldYielder(f'{input_path}/{input_filename}')
+            df = fy.get_df(inc_inputs=True, deprocess=False, nan_to_num=False, verbose=False, suppress_warn=True)
+            for f in misc_features: # add misc features
+                df[f] = fy.get_column(f)
+            df['fold_id'] = df[xtrain_split_feature] % n_splits
 
-        # run cross-inference for folds
-        pred_dict = predict_folds(df, train_features, misc_features, 'fold_id', models)
+            # run cross-inference for folds
+            pred_dict = predict_folds(df, train_features, misc_features, 'fold_id', models)
 
-        # store predictions in RDataFrame and snapshot it into output ROOT file
-        print(f"        storing to output file")
-        output_filename = fill_placeholders(cfg.output_filename_template, {'{sample_name}': sample_name, '{year}': cfg.year})
-        if os.path.exists(f'{output_path}/{output_filename}'):
-            os.system(f'rm {output_path}/{output_filename}')
-        R_df = R.RDF.MakeNumpyDataFrame(pred_dict)
-        R_df.Snapshot(cfg.output_tree_name, f'{output_path}/{output_filename}')
-        del(df, R_df); gc.collect()
+            if cfg.kind == 'for_datacards':
+                # store predictions in RDataFrame and snapshot it into output ROOT file
+                print(f"        storing to output file")
+                output_filename = fill_placeholders(cfg.output_filename_template, {'{sample_name}': sample_name, '{year}': cfg.year})
+                if os.path.exists(f'{output_path}/{output_filename}'):
+                    os.system(f'rm {output_path}/{output_filename}')
+                R_df = R.RDF.MakeNumpyDataFrame(pred_dict)
+                R_df.Snapshot(cfg.output_tree_name, f'{output_path}/{output_filename}')
+                del(df, R_df); gc.collect()
+            elif cfg.kind == 'for_evaluation':
+                print(f"        storing to output file & log to mlflow")
+                df_pred = pd.DataFrame(pred_dict)
+                df_pred.to_csv(f'{output_path}/{output_filename}.csv')
+                mlflow.log_artifact(f'{output_path}/{output_filename}.csv', artifact_path='pred')
+            else:
+                raise Exception(f'Unknown kind for prediction: {cfg.kind}')
+
+        mlflow.log_params({'run_pred_on': f'{input_path}/{cfg.input_filename_template}',
+                           'saved_pred_to': f'{output_path}/{cfg.output_filename_template}'
+        })
 
 if __name__ == '__main__':
     main()
