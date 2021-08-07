9ba7cde98be3f460f0b8e65852a9ee7b2e069563
yaourtpourtoi <oleg.filatov@phystech.edu>
2021-08-07 21:42:28 +0200

    generalise prediction to one fold

--- predict.py
+++ predict.py
@@ -6,9 +6,7 @@ from hydra.utils import to_absolute_path
 from omegaconf import OmegaConf, DictConfig
 
 import ROOT as R
-import uproot
 from lumin.nn.data.fold_yielder import FoldYielder
-import numpy as np
 import pandas as pd
 import mlflow
 
@@ -33,6 +31,7 @@ def main(cfg: DictConfig) -> None:
         model_cfg = yaml.safe_load(f)
     for s in model_cfg['signature']['inputs'].split('{')[1:]:
         train_features.append(s.split("\"")[3])
+    fold_id_column = 'fold_id'
 
     mlflow.set_tracking_uri(f"file://{to_absolute_path('mlruns')}")
     with mlflow.start_run(run_id=cfg.mlflow_runID):
@@ -47,31 +46,27 @@ def main(cfg: DictConfig) -> None:
             df = fy.get_df(inc_inputs=True, deprocess=False, nan_to_num=False, verbose=False, suppress_warn=True)
             for f in misc_features: # add misc features
                 df[f] = fy.get_column(f)
-            df['fold_id'] = df[xtrain_split_feature] % n_splits
+            df[fold_id_column] = (df[xtrain_split_feature] % n_splits).astype('int32')
 
             # run cross-inference for folds
-            pred_dict = predict_folds(df, train_features, misc_features, 'fold_id', models)
+            pred_dict = predict_folds(df, train_features, misc_features, fold_id_column=fold_id_column, models=models)
 
+            print(f"        storing to output file")
+            output_filename = fill_placeholders(cfg.output_filename_template, {'{sample_name}': sample_name, '{year}': cfg.year})
+            if os.path.exists(f'{output_path}/{output_filename}'):
+                os.system(f'rm {output_path}/{output_filename}')
             if cfg.kind == 'for_datacards':
                 # store predictions in RDataFrame and snapshot it into output ROOT file
-                print(f"        storing to output file")
-                output_filename = fill_placeholders(cfg.output_filename_template, {'{sample_name}': sample_name, '{year}': cfg.year})
-                if os.path.exists(f'{output_path}/{output_filename}'):
-                    os.system(f'rm {output_path}/{output_filename}')
                 R_df = R.RDF.MakeNumpyDataFrame(pred_dict)
                 R_df.Snapshot(cfg.output_tree_name, f'{output_path}/{output_filename}')
                 del(df, R_df); gc.collect()
             elif cfg.kind == 'for_evaluation':
-                print(f"        storing to output file & log to mlflow")
                 df_pred = pd.DataFrame(pred_dict)
-                df_pred.to_csv(f'{output_path}/{output_filename}.csv')
-                mlflow.log_artifact(f'{output_path}/{output_filename}.csv', artifact_path='pred')
+                df_pred.to_csv(f'{output_path}/{output_filename}')
+                mlflow.log_artifact(f'{output_path}/{output_filename}', artifact_path='pred')
+                del(df_pred); gc.collect()
             else:
                 raise Exception(f'Unknown kind for prediction: {cfg.kind}')
-
-        mlflow.log_params({'run_pred_on': f'{input_path}/{cfg.input_filename_template}',
-                           'saved_pred_to': f'{output_path}/{cfg.output_filename_template}'
-        })
 
 if __name__ == '__main__':
     main()
--- train.py
+++ train.py
@@ -3,7 +3,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 from sklearn.model_selection import LeaveOneGroupOut, ShuffleSplit
-from sklearn.metrics import accuracy_score, log_loss
 import lightgbm as lgb
 import mlflow
 import mlflow.lightgbm
@@ -16,11 +15,10 @@ from hydra.utils import to_absolute_path
 from omegaconf import OmegaConf, DictConfig
 
 from utils.processing import fill_placeholders
-from utils.plotting import plot_class_score
 
 @hydra.main(config_path="configs", config_name="train")
 def main(cfg: DictConfig) -> None:
-    # prepare train data
+    # load training data into DataFrame + add necessary columns
     print('\n--> Loading training data')
     train_file = fill_placeholders(to_absolute_path(cfg.train_file), {'{year}': cfg.year})
     train_fy = FoldYielder(train_file)
@@ -28,27 +26,28 @@ def main(cfg: DictConfig) -> None:
     train_df['w_cp'] = train_fy.get_column('w_cp')
     train_df['w_class_imbalance'] = train_fy.get_column('w_class_imbalance')
 
-    # fetch feature/weight/target names
+    # define feature/weight/target names
     train_features = cfg.cont_features + cfg.cat_features # features to be used in training
     weight_name = cfg.weight_name
     target_name = 'gen_target' # internal target name defined inside of lumin library
+    fold_id_column = 'fold_id'
 
     if cfg.n_splits > 1:
         assert type(cfg.n_splits)==int
         train_df[cfg.xtrain_split_feature] = train_fy.get_column(cfg.xtrain_split_feature)
-        train_df['fold_id'] = (train_df[cfg.xtrain_split_feature] % cfg.n_splits).astype('int32')
+        train_df[fold_id_column] = (train_df[cfg.xtrain_split_feature] % cfg.n_splits).astype('int32')
 
         # check that there is no more that 5% difference between folds in terms of number of entries
-        fold_id_count_diff = np.std(train_df['fold_id'].value_counts()) / np.mean(train_df['fold_id'].value_counts())
+        fold_id_count_diff = np.std(train_df[fold_id_column].value_counts()) / np.mean(train_df[fold_id_column].value_counts())
         if fold_id_count_diff > 0.05:
             raise Exception(f'Observed {fold_id_count_diff * 100}% relative difference in number of entries across folds. Please check that the split is done equally.')
 
         print(f'\n[INFO] Will split training data set into ({cfg.n_splits}) folds over values of ({cfg.xtrain_split_feature}) feature to perform cross-training')
         splitter = LeaveOneGroupOut()
-        idx_yielder = splitter.split(train_df, groups=train_df['fold_id'])
+        idx_yielder = splitter.split(train_df, groups=train_df[fold_id_column])
     elif cfg.n_splits == 1:
         print(f'\n[INFO] Will train a single model on ({cfg.train_size}) part of the training data set with the rest used for validation')
-        train_df['fold_id'] = 0
+        train_df[fold_id_column] = 0
         splitter = ShuffleSplit(n_splits=1, train_size=cfg.train_size, random_state=1357)
         idx_yielder = splitter.split(train_df)
     else:
@@ -71,7 +70,7 @@ def main(cfg: DictConfig) -> None:
             validation_fold_df = train_df.iloc[validation_idx]
 
             # check that `i_fold` is the same as fold ID corresponding to a validation fold
-            validation_fold_idx = set(validation_fold_df['fold_id'])
+            validation_fold_idx = set(validation_fold_df[fold_id_column])
             assert len(validation_fold_idx)==1 and i_fold in validation_fold_idx
 
             # construct lightgbm dataset
