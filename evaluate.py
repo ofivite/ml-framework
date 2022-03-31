9bb00e193533f1ee22254a78d1b40c674376ffc8
Oleg Filatov <20295221+yaourtpourtoi@users.noreply.github.com>
2022-03-31 16:53:08 +0200

    change cfg key reference

--- evaluate.py
+++ evaluate.py
@@ -13,56 +13,56 @@ from utils.plotting import plot_class_score, plot_curves
 @hydra.main(config_path="configs", config_name="evaluate")
 def main(cfg: DictConfig) -> None:
     print('\n--> Loading predictions')
-    run_folder = to_absolute_path(f'mlruns/{cfg.experiment_id}/{cfg.run_id}/')
-    df_pred = pd.read_csv(f'{run_folder}/artifacts/pred/{cfg.dataset}.csv')
+    run_folder = to_absolute_path(f'mlruns/{cfg["experiment_id"]}/{cfg["run_id"]}/')
+    df_pred = pd.read_csv(f'{run_folder}/artifacts/pred/{cfg["dataset"]}.csv')
 
     # check that class id match in data and in training cfg
-    class_ids = {int(class_id) for class_id in cfg.class_to_info}
+    class_ids = {int(class_id) for class_id in cfg["class_to_info"]}
     assert set(df_pred['target']) == class_ids
     class_names = []
 
     mlflow.set_tracking_uri(f"file://{to_absolute_path('mlruns')}")
-    with mlflow.start_run(experiment_id=cfg.experiment_id, run_id=cfg.run_id):
+    with mlflow.start_run(experiment_id=cfg["experiment_id"], run_id=cfg["run_id"]):
         # plot density scores in each category
-        for class_id in cfg.class_to_info:
-            class_name = cfg.class_to_info[class_id]['name']
+        for class_id in cfg["class_to_info"]:
+            class_name = cfg["class_to_info"][class_id]['name']
             class_names.append(class_name)
 
             print(f'\n--> Plotting density for class ({class_name})')
             fig_density_name = f'density_{class_name}.pdf'
-            fig_density = plot_class_score(df_pred, class_id, cfg.class_to_info, how='density')
+            fig_density = plot_class_score(df_pred, class_id, cfg["class_to_info"], how='density')
             fig_density.write_image(fig_density_name)
-            mlflow.log_figure(fig_density, f'plots/{cfg.dataset}/density_{class_name}.html')
-            mlflow.log_artifact(fig_density_name, f'plots/{cfg.dataset}/pdf')
+            mlflow.log_figure(fig_density, f'plots/{cfg["dataset"]}/density_{class_name}.html')
+            mlflow.log_artifact(fig_density_name, f'plots/{cfg["dataset"]}/pdf')
             os.remove(fig_density_name)
-            # fig_stacked = plot_class_score(df_pred, class_id, cfg.class_to_info, how='stacked', weight='plot_weight')
+            # fig_stacked = plot_class_score(df_pred, class_id, cfg["class_to_info"], how='stacked', weight='plot_weight')
 
         # make confusion matrix
         print(f'\n--> Producing confusion matrix')
         for confusion_norm in ['true', 'pred']:
             cm = confusion_matrix(df_pred['target'], df_pred['pred_class'], normalize=confusion_norm, sample_weight=df_pred['w_class_imbalance'])
             disp = ConfusionMatrixDisplay(cm, display_labels=class_names)
-            for class_id in cfg.class_to_info:
-                mlflow.log_metric(f'cm_{class_id}{class_id}_{confusion_norm} / {cfg.dataset}', cm[class_id,class_id])
+            for class_id in cfg["class_to_info"]:
+                mlflow.log_metric(f'cm_{class_id}{class_id}_{confusion_norm} / {cfg["dataset"]}', cm[class_id,class_id])
 
             fig, ax = plt.subplots(figsize=(10, 9))
             disp.plot(cmap='Blues', ax=ax)
             cm_name = f'confusion_matrix_{confusion_norm}.pdf'
             ax.set_title(f'Confusion matrix: class balanced, normalize={confusion_norm}')
             fig.savefig(cm_name)
-            mlflow.log_artifact(cm_name, f'plots/{cfg.dataset}/pdf')
+            mlflow.log_artifact(cm_name, f'plots/{cfg["dataset"]}/pdf')
             os.remove(cm_name)
 
         # plot ROC and precision-recall curves for each class
         print(f'\n--> Plotting ROC & PR curves')
-        for curve_name, curve_data in plot_curves(df_pred, cfg.class_to_info).items():
+        for curve_name, curve_data in plot_curves(df_pred, cfg["class_to_info"]).items():
             curve_data['figure'].write_image(f'{curve_name}_curve.pdf')
-            mlflow.log_figure(curve_data['figure'], f'plots/{cfg.dataset}/{curve_name}_curve.html')
+            mlflow.log_figure(curve_data['figure'], f'plots/{cfg["dataset"]}/{curve_name}_curve.html')
             for class_name in class_names:
                 for metric_key in curve_data:
                     if ('auc' in metric_key or 'average_prec' in metric_key):
-                        mlflow.log_metric(f'{curve_name}_{metric_key} / {cfg.dataset}', curve_data[metric_key])
-            mlflow.log_artifact(f'{curve_name}_curve.pdf', f'plots/{cfg.dataset}/pdf')
+                        mlflow.log_metric(f'{curve_name}_{metric_key} / {cfg["dataset"]}', curve_data[metric_key])
+            mlflow.log_artifact(f'{curve_name}_curve.pdf', f'plots/{cfg["dataset"]}/pdf')
             os.remove(f'{curve_name}_curve.pdf')
     print()
 
--- predict.py
+++ predict.py
@@ -16,13 +16,13 @@ from utils.inference import load_models, predict_folds
 @hydra.main(config_path="configs/predict")
 def main(cfg: DictConfig) -> None:
     # fill placeholders in the cfg parameters
-    input_path = to_absolute_path(cfg.input_path)
-    run_folder = to_absolute_path(f'mlruns/{cfg.experiment_id}/{cfg.run_id}/')
+    input_path = to_absolute_path(cfg["input_path"])
+    run_folder = to_absolute_path(f'mlruns/{cfg["experiment_id"]}/{cfg["run_id"]}/')
     models, n_splits, xtrain_split_feature = load_models(run_folder)
 
     # extract names of training features from mlflow-stored model
     # note: not checking that the set of features is the same across models
-    misc_features = OmegaConf.to_object(cfg.misc_features)
+    misc_features = OmegaConf.to_object(cfg["misc_features"])
     train_features = []
     with open(to_absolute_path(f'{run_folder}/artifacts/model_0/MLmodel'), 'r') as f:
         model_cfg = yaml.safe_load(f)
@@ -31,12 +31,12 @@ def main(cfg: DictConfig) -> None:
     fold_id_column = 'fold_id'
 
     mlflow.set_tracking_uri(f"file://{to_absolute_path('mlruns')}")
-    with mlflow.start_run(experiment_id=cfg.experiment_id, run_id=cfg.run_id):
+    with mlflow.start_run(experiment_id=cfg["experiment_id"], run_id=cfg["run_id"]):
         # loop over input samples
-        for sample_name in cfg.sample_names:
+        for sample_name in cfg["sample_names"]:
             print(f'\n--> Predicting {sample_name}')
             print(f"        loading data set")
-            input_filename = fill_placeholders(cfg.input_filename_template, {'{sample_name}': sample_name})
+            input_filename = fill_placeholders(cfg["input_filename_template"], {'{sample_name}': sample_name})
 
             # read DataFrame from input file
             df = read_hdf(f'{input_path}/{input_filename}', key_list=['cont_features', 'cat_features', 'misc_features', 'targets'])
@@ -46,10 +46,10 @@ def main(cfg: DictConfig) -> None:
             pred_dict = predict_folds(df, train_features, misc_features, fold_id_column=fold_id_column, models=models)
 
             print(f"        storing to output file")
-            output_filename = fill_placeholders(cfg.output_filename_template, {'{sample_name}': sample_name})
-            if cfg.kind == 'for_datacards':                
+            output_filename = fill_placeholders(cfg["output_filename_template"], {'{sample_name}': sample_name})
+            if cfg["kind"] == 'for_datacards':                
                 # extract original index
-                orig_filename = fill_placeholders(to_absolute_path(f'{cfg.orig_path}/{cfg.orig_filename_template}'), {'{sample_name}': sample_name})
+                orig_filename = fill_placeholders(to_absolute_path(f'{cfg["orig_path"]}/{cfg["orig_filename_template"]}'), {'{sample_name}': sample_name})
                 with uproot.open(orig_filename) as f:
                     t = f['TauCheck']
                     orig_index = t.arrays(['evt', 'run'], library='pd')
@@ -62,16 +62,16 @@ def main(cfg: DictConfig) -> None:
 
                 # store predictions in RDataFrame and snapshot it into output ROOT file
                 R_df = R.RDF.MakeNumpyDataFrame(pred_dict)
-                R_df.Snapshot(cfg.output_tree_name, output_filename)
+                R_df.Snapshot(cfg["output_tree_name"], output_filename)
                 mlflow.log_artifact(output_filename, artifact_path='pred')
                 del(df, R_df); os.remove(output_filename); gc.collect()
-            elif cfg.kind == 'for_evaluation':
+            elif cfg["kind"] == 'for_evaluation':
                 df_pred = pd.DataFrame(pred_dict)
                 df_pred.to_csv(output_filename, index=False)
                 mlflow.log_artifact(output_filename, artifact_path='pred')
                 del(df_pred); os.remove(output_filename); gc.collect()
             else:
-                raise Exception(f'Unknown kind for prediction: {cfg.kind}')
+                raise Exception(f'Unknown kind for prediction: {cfg["kind"]}')
         print()
         
 if __name__ == '__main__':
