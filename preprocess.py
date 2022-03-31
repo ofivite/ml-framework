55dd9841b9f386974cffd64e82b3702f50d0bb75
Oleg Filatov <20295221+yaourtpourtoi@users.noreply.github.com>
2022-03-31 16:43:14 +0200

    change cfg key referencing

--- preprocess.py
+++ preprocess.py
@@ -20,38 +20,38 @@ from utils.processing import fill_placeholders
 
 @hydra.main(config_path="configs/preprocess")
 def main(cfg: DictConfig) -> None:
-    cont_features = OmegaConf.to_object(cfg.cont_features) if cfg.cont_features is not None else None
-    cat_features = OmegaConf.to_object(cfg.cat_features) if cfg.cat_features is not None else None
-    misc_features = OmegaConf.to_object(cfg.misc_features) if cfg.misc_features is not None else None
-    input_branches = OmegaConf.to_object(cfg.input_branches)
-    output_path = to_absolute_path(cfg.output_path)
+    cont_features = OmegaConf.to_object(cfg["cont_features"]) if cfg["cont_features"] is not None else None
+    cat_features = OmegaConf.to_object(cfg["cat_features"]) if cfg["cat_features"] is not None else None
+    misc_features = OmegaConf.to_object(cfg["misc_features"]) if cfg["misc_features"] is not None else None
+    input_branches = OmegaConf.to_object(cfg["input_branches"])
+    output_path = to_absolute_path(cfg["output_path"])
     os.makedirs(output_path, exist_ok=True)
 
     # combine all input data nodes into a single pandas dataframe
     data_samples = []
     _target = 'target' # internal target name
-    for sample in cfg.input_samples:
-        if cfg.for_training:
+    for sample in cfg["input_samples"]:
+        if cfg["for_training"]:
             assert len(sample)==1 # for training, sample is a dictionary with 1 element, see the cfg file
             sample_name = list(sample.keys())[0]
         else:
             assert type(sample)==str
             sample_name = sample
-        input_filename = fill_placeholders(cfg.input_filename_template, {'{sample_name}': sample_name})
+        input_filename = fill_placeholders(cfg["input_filename_template"], {'{sample_name}': sample_name})
         print(f'\n--> Opening {sample_name}...')
-        with uproot.open(f'{cfg.input_path}/{input_filename}') as f:
-            if cfg.for_training:
+        with uproot.open(f'{cfg["input_path"]}/{input_filename}') as f:
+            if cfg["for_training"]:
                 processes = list(sample.values())[0]
                 for process_name, process_cfg in processes.items():
                     print(f'    loading {process_name}')
-                    data_sample = f[cfg.input_tree_name].arrays(input_branches, cut=process_cfg['cut'], library='pd')
+                    data_sample = f[cfg["input_tree_name"]].arrays(input_branches, cut=process_cfg['cut'], library='pd')
                     if data_sample.shape[0]==0:
                         raise RuntimeError(f'DataFrame for process ({process_name}) is empty')
                     data_sample['group_name'] = process_name
                     data_sample[_target] = process_cfg['class']
                     data_samples.append(data_sample)
             else:
-                data_sample = f[cfg.input_tree_name].arrays(input_branches, cut=None, library='pd')
+                data_sample = f[cfg["input_tree_name"]].arrays(input_branches, cut=None, library='pd')
                 if data_sample.shape[0]==0:
                     raise RuntimeError(f'DataFrame for file ({input_filename}) is empty')
                 data_sample['group_name'] = sample_name
@@ -74,15 +74,15 @@ def main(cfg: DictConfig) -> None:
     data['njets'] = data.njets.clip(0, 5)
 
     # split data into output nodes: either train+test (for training) or sample_name based splitting (for prediction)
-    if cfg.for_training:
+    if cfg["for_training"]:
         # derive key for stratified split
         strat_key = 'strat_key'
         data[strat_key] = ids2unique(data[[_target]].values)
         # data[strat_key] = ids2unique(data[[_target] + cat_features].values)
 
         # split into output_samples[0] -> train, output_samples[1] -> test
-        output_samples = train_test_split(data, train_size=cfg.train_size, stratify=data[strat_key], random_state=1357)
-        output_sample_names = cfg.output_samples
+        output_samples = train_test_split(data, train_size=cfg["train_size"], stratify=data[strat_key], random_state=1357)
+        output_sample_names = cfg["output_samples"]
         assert len(output_sample_names)==len(output_samples)
 
         # create preprocessing pipe
@@ -101,21 +101,21 @@ def main(cfg: DictConfig) -> None:
         
         # fit, transform and save pipeline
         input_pipe.fit(X=output_samples[0][cont_features].values.astype('float32')) # use only training data & continuous feats for that
-        with open(f'{output_path}/{cfg.pipe_name}.pkl', 'wb') as fout: pickle.dump(input_pipe, fout)
+        with open(f'{output_path}/{cfg["pipe_name"]}.pkl', 'wb') as fout: pickle.dump(input_pipe, fout)
         
     else:
         strat_key = None # no stratification for prediction
         outputs = {name: group for name, group in data.groupby('group_name')}
         output_samples = outputs.values()
         output_sample_names = outputs.keys()
-        output_sample_names = [fill_placeholders(cfg.output_filename_template, {'{sample_name}': n}) for n in output_sample_names]
+        output_sample_names = [fill_placeholders(cfg["output_filename_template"], {'{sample_name}': n}) for n in output_sample_names]
 
         # fetch already fitted pipe
-        with open(to_absolute_path(cfg.input_pipe_file), 'rb') as f:
+        with open(to_absolute_path(cfg["input_pipe_file"]), 'rb') as f:
             input_pipe = pickle.load(f)
 
     # derive training weights as of CP analysis (based on the whole input data)
-    if cfg.for_training and cfg.compute_w_CP:
+    if cfg["for_training"] and cfg["compute_w_CP"]:
         class_weight_map = {}
         for class_label in set(data[_target]):
             class_weight_map[class_label] = np.sum(data['weight'])/np.sum(data.query(f'{_target} == {class_label}')['weight'])
@@ -125,14 +125,14 @@ def main(cfg: DictConfig) -> None:
     for output_sample_name, output_sample in zip(output_sample_names, output_samples):
         print(f'    {output_sample_name}')
         # derive class imbalance weights (per output node: train/test)
-        if cfg.for_training:
+        if cfg["for_training"]:
             w_class_imbalance_map = {}
             for class_label in set(output_sample[_target]):
                 w_class_imbalance_map[class_label] = len(output_sample)/len(output_sample.query(f'{_target}=={class_label}'))
 
             # add training weights accounting for imbalance in data
             output_sample['w_class_imbalance'] = output_sample[_target].map(w_class_imbalance_map)
-            if cfg.compute_w_CP:
+            if cfg["compute_w_CP"]:
                 output_sample['class_weight'] = output_sample[_target].map(class_weight_map)
                 output_sample['w_cp'] = abs(output_sample['weight'])*output_sample['class_weight']
 
--- train.py
+++ train.py
@@ -16,56 +16,56 @@ from utils.processing import read_hdf
 def main(cfg: DictConfig) -> None:
     # load groups into DataFrame
     print('\n--> Loading training data')
-    train_file = to_absolute_path(cfg.train_file)
+    train_file = to_absolute_path(cfg["train_file"])
     train_df = read_hdf(train_file, key_list=['cont_features', 'cat_features', 'misc_features', 'targets'])
 
     # define training features/weights/targets
     train_features = []  
-    if cfg.cont_features is not None:
-        train_features += cfg.cont_features 
-    if cfg.cat_features is not None:
-        train_features += cfg.cat_features
+    if cfg["cont_features"] is not None:
+        train_features += cfg["cont_features"] 
+    if cfg["cat_features"] is not None:
+        train_features += cfg["cat_features"]
     if len(train_features) == 0:
         raise RuntimeError('Both continuous and categorical features are None')
 
-    weight_name = cfg.weight_name
+    weight_name = cfg["weight_name"]
     target_name = 'target' # internal target name defined at preprocessing step
     fold_id_column = 'fold_id'
 
-    if cfg.n_splits > 1:
-        assert type(cfg.n_splits)==int
-        split_feature_values = train_df[cfg.xtrain_split_feature].values
-        train_df[fold_id_column] = (split_feature_values % cfg.n_splits).astype('int32')
+    if cfg["n_splits"] > 1:
+        assert type(cfg["n_splits"])==int
+        split_feature_values = train_df[cfg["xtrain_split_feature"]].values
+        train_df[fold_id_column] = (split_feature_values % cfg["n_splits"]).astype('int32')
 
         # check that there is no more that 5% difference between folds in terms of number of entries
         fold_id_count_diff = np.std(train_df[fold_id_column].value_counts()) / np.mean(train_df[fold_id_column].value_counts())
         if fold_id_count_diff > 0.05:
             raise Exception(f'Observed {fold_id_count_diff * 100}% relative difference in number of entries across folds. Please check that the split is done equally.')
 
-        print(f'\n[INFO] Will split training data set into ({cfg.n_splits}) folds over values of ({cfg.xtrain_split_feature}) feature to perform cross-training')
+        print(f'\n[INFO] Will split training data set into ({cfg["n_splits"]}) folds over values of ({cfg["xtrain_split_feature"]}) feature to perform cross-training')
         splitter = LeaveOneGroupOut()
         idx_yielder = splitter.split(train_df, groups=train_df[fold_id_column])
-    elif cfg.n_splits == 1:
-        print(f'\n[INFO] Will train a single model on ({cfg.train_size}) part of the training data set with the rest used for validation')
+    elif cfg["n_splits"] == 1:
+        print(f'\n[INFO] Will train a single model on ({cfg["train_size"]}) part of the training data set with the rest used for validation')
         train_df[fold_id_column] = 0
-        splitter = ShuffleSplit(n_splits=1, train_size=cfg.train_size, random_state=1357)
+        splitter = ShuffleSplit(n_splits=1, train_size=cfg["train_size"], random_state=1357)
         idx_yielder = splitter.split(train_df)
     else:
-        raise ValueError(f'n_splits should be positive integer, got {cfg.n_splits}')
+        raise ValueError(f'n_splits should be positive integer, got {cfg["n_splits"]}')
 
     with mlflow.start_run() as active_run:
         # enable auto logging for mlflow & log some cfg parameters
         mlflow.lightgbm.autolog(log_models=False)
         mlflow.log_params({
             'train_file': train_file,
-            'xtrain_split_feature': cfg.xtrain_split_feature,
-            'weight_name': cfg.weight_name,
+            'xtrain_split_feature': cfg["xtrain_split_feature"],
+            'weight_name': cfg["weight_name"],
             'target_name': target_name
         })
 
         print(f'\n--> Training model...')
         for i_fold, (train_idx, validation_idx) in enumerate(idx_yielder):
-            if cfg.n_splits > 1: print(f'\n\n    leaving fold {i_fold} out\n\n')
+            if cfg["n_splits"] > 1: print(f'\n\n    leaving fold {i_fold} out\n\n')
             train_fold_df = train_df.iloc[train_idx]
             validation_fold_df = train_df.iloc[validation_idx]
 
@@ -77,15 +77,15 @@ def main(cfg: DictConfig) -> None:
             train_data = lgb.Dataset(train_fold_df[train_features],
                                      label=train_fold_df[target_name],
                                      weight=train_fold_df[weight_name],
-                                     categorical_feature=cfg.cat_features)
+                                     categorical_feature=cfg["cat_features"])
             validation_data = lgb.Dataset(validation_fold_df[train_features],
                                           label=validation_fold_df[target_name],
                                           weight=validation_fold_df[weight_name],
-                                          categorical_feature=cfg.cat_features,
+                                          categorical_feature=cfg["cat_features"],
                                           reference=train_data)
 
             # train booster
-            model = lgb.train(OmegaConf.to_object(cfg.model_param),
+            model = lgb.train(OmegaConf.to_object(cfg["model_param"]),
                               train_data,
                               valid_sets=[train_data, validation_data], valid_names=[f'train_{i_fold}', f'valid_{i_fold}'])
 
