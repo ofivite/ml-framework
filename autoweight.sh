c0792f67a6cebc92574a2dd02bd6f3a18e36fd9b
Agni Keyoor Purani <agni.purani@gmail.com>
2022-06-14 20:09:26 +0200

    Minor fixes

--- autoweight.sh
+++ autoweight.sh
@@ -1,5 +1,5 @@
 echo ${SHELL}
-
+EXPERIMENTID=7
 for var in "$@"
 do
     echo "Doing ${var}"
@@ -7,5 +7,9 @@ do
     echo " "
     sed -e "s;%BGWEIGHT%;$var;g" configs/preprocess/training_data/lowpT_Taus.txt > configs/preprocess/training_data/lowpT_Taus.yaml
     python preprocess.py --config-path configs/preprocess/training_data --config-name lowpT_Taus.yaml year=2022
-    mlflow run --experiment-id 6 --no-conda .
+    d=`mlflow run --experiment-id $EXPERIMENTID --no-conda . 2>&1 >out_$var.txt | grep "=== Run (ID "`
+    d=$(sed -n "s/^.*'\(.*\)'.*$/\1/p" <<< $d)
+    python predict.py --config-name for_evaluation.yaml year=2022 experiment_id=$EXPERIMENTID run_id=$d
+    python evaluate.py experiment_id=$EXPERIMENTID run_id=$d dataset=test
+    python evaluate.py experiment_id=$EXPERIMENTID run_id=$d dataset=train
 done
